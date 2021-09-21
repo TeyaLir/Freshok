f2d4a56cd3439274d3785c1471cbdff281413a31
TeyaLir <ia_88@bk.ru>
2021-09-21 22:00:12 +0300

    Add header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
+
+  // Каталог продуктов
+  $('.product-catalog__title').on('click', function () {
+    $('.product-catalog__list').toggleClass('product-catalog__list--active');
+    $('.product-catalog__title').toggleClass('product-catalog__title--active');
+  });
   
 });
