1e424094efbfdd3d7ea441036fcab97dd90809f1
TeyaLir <ia_88@bk.ru>
2021-09-23 19:10:24 +0300

    Add top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -5,5 +5,13 @@ $(function(){
     $('.product-catalog__list').toggleClass('product-catalog__list--active');
     $('.product-catalog__title').toggleClass('product-catalog__title--active');
   });
+
+  // top-slider
+  $('.top-slider__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="стрелка влево"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="стрелка вправо"></button>',
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
   
 });
