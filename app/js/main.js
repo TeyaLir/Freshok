2ba5941295a6d7d7cdc38c65671a66903a8f8406
TeyaLir <ia_88@bk.ru>
2021-09-27 14:40:42 +0300

    Add benefits section

--- app/js/main.js
+++ app/js/main.js
@@ -10,8 +10,8 @@ $(function(){
   $('.top-slider__slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="стрелка влево"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="стрелка вправо"></button>',
-    autoplay: true,
-    autoplaySpeed: 5000
+    // autoplay: true,
+    // autoplaySpeed: 5000,
   });
   
 });
