7385db0791305abf0b81bb5a0b7f91b05532efaa
TeyaLir <ia_88@bk.ru>
2021-10-04 19:06:45 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,18 @@
 $(function(){
 
   // Каталог продуктов
-  $('.product-catalog__title').on('click', function () {
+  $('.product-catalog__btn').on('click', function () {
     $('.product-catalog__list').toggleClass('product-catalog__list--active');
-    $('.product-catalog__title').toggleClass('product-catalog__title--active');
+    $('.product-catalog__btn').toggleClass('product-catalog__btn--active');
   });
 
   // top-slider
   $('.top-slider__slider').slick({
-    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="стрелка влево"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="стрелка вправо"></button>',
     // autoplay: true,
     // autoplaySpeed: 5000,
   });
 
   // MixItUp для топовых товаров
-  var mixer = mixitup('.goods__content');
+  var mixer = mixitup('.goods-top__content');
   
 });
