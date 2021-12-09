669f17a3e3b79fdcbd88a31fb52b78ffa44522f6
TeyaLir <ia_88@bk.ru>
2021-12-09 16:07:47 +0300

    Add Product page

--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,7 @@ function scripts() {
     'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'node_modules/simplebar/dist/simplebar.js',
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
